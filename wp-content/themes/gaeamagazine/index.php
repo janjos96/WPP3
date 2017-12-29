70fd33def2df81d4c4024b5eb85b8d290fb2a222
Janjos <joaoanjos_96@hotmail.com>
2017-12-29 21:37:40 +0000

    theme

---
+++ wp-content/themes/gaeamagazine/index.php
@@ -0,0 +1,144 @@
+<!doctype html>
+<html class="no-js" lang="pt">
+
+<?php include 'parts/head.php' ?>
+
+<body>
+
+<div class="scrollbar" id="style-3">
+    <div class="force-overflow">
+
+
+<div class="row">
+    <div class="col-2">
+        <?php include 'parts/navbar.php' ?>
+    </div>
+    <div class="col-10">
+      <!--  <div id="language"><a>PT</a>/<a>ENG</a></div>-->
+
+
+        <img id="imagemain" src="img/main.png" alt="main">
+
+
+
+
+
+
+    <section id="latestarticles" >
+
+            <h1>LATEST ARTICLES</h1>
+
+            <div class="grid">
+                <div class="grid-item grid-item--width2">
+
+                        <div class="squarecontent">Apocalipse Juggernaut, Hello!<br><br>
+                            <div class="viewmore">
+                                I tend to view the whole disaster as an opportunity to try stuff.<br><br>
+
+                                <a href="article.php" >View More</a>
+                            </div>
+                        </div>
+                </div>
+
+
+                <div class="grid-item grid-item--height2">
+                    <div class="squarecontent">A year from Monday<br><br>
+                        <div class="viewmore">
+                            The question is: Is my thought changing? It is and it
+                            isn't.<br><br>
+
+                            <a href="article2.php" >View More</a>
+                        </div>
+                    </div>
+
+                </div>
+
+
+
+
+                <div class="grid-item grid-item--width2 grid-item--height2"><a class="viewmore" href="article3.php">Patterns in Nature</a></div>
+                <div class="grid-item grid-item--width2 grid-item--height2"><a class="viewmore">view more</a></div>
+                <div class="grid-item--width2 grid-item--height2"><a class="viewmore">view more</a></div>
+                <div class="grid-item grid-item--width2"><a class="viewmore">view more</a></div>
+                <div class="grid-item grid-item--width2"><a class="viewmore">view more</a></div>
+                <div class="grid-item grid-item--height2"><a class="viewmore">view more</a></div>
+                <div class="grid-item grid-item--width2"><a class="viewmore">view more</a></div>
+                <div class="grid-item grid-item--width2"><a class="viewmore">view more</a></div>
+                <div class="grid-item grid-item--height2"><a class="viewmore">view more</a></div>
+                <div class="grid-item grid-item--width2 grid-item--height2"><a class="viewmore">view more</a></div>
+                <div class="grid-item grid-item--height2"><a class="viewmore">view more</a></div>
+                <div class="grid-item grid-item--width2"><a class="viewmore">view more</a></div>
+                <div class="grid-item grid-item--height2"><a class="viewmore">view more</a></div>
+
+            </div>
+
+
+
+
+    </section>
+
+    <section id="latesteditions">
+        <h1>EDITIONS</h1>
+
+        <div class="grid">
+            <div class="grid-item grid-item--width2"></div>
+            <div class="grid-item grid-item--height2"></div>
+            <div class="grid-item"></div>
+            <div class="grid-item"></div>
+            <div class="grid-item grid-item--width2 grid-item--height2"></div>
+            <div class="grid-item grid-item--width2"></div>
+            <div class="grid-item grid-item--width2"></div>
+            <div class="grid-item grid-item--height2"></div>
+            <div class="grid-item"></div>
+            <div class="grid-item grid-item--width2"></div>
+            <div class="grid-item grid-item--height2"></div>
+            <div class="grid-item"></div>
+            <div class="grid-item"></div>
+            <div class="grid-item grid-item--width2"></div>
+            <div class="grid-item grid-item--height2"></div>
+            <div class="grid-item"></div>
+            <div class="grid-item grid-item--width2"></div>
+            <div class="grid-item grid-item--height2"></div>
+            <div class="grid-item"></div>
+            <div class="grid-item"></div>
+        </div>
+
+
+    </section>
+    <footer>
+        <p>GAEA® is a registered trademark.</p>
+
+    </footer>
+
+
+
+<!--[if lte IE 9]>
+<p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="https://browsehappy.com/">upgrade
+    your browser</a> to improve your experience and security.</p>
+<![endif]-->
+
+<!-- Add your site or application content here -->
+<script src="js/vendor/modernizr-3.5.0.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.2.1.min.js"
+        integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
+<script>window.jQuery || document.write('<script src="js/vendor/jquery-3.2.1.min.js"><\/script>')</script>
+<script src="js/plugins.js"></script>
+<script src="js/isotope.pkgd.min.js"></script>
+
+<!-- Google Analytics: change UA-XXXXX-Y to be your site's ID. -->
+<script>
+    window.ga = function () {
+        ga.q.push(arguments)
+    };
+    ga.q = [];
+    ga.l = +new Date;
+    ga('create', 'UA-XXXXX-Y', 'auto');
+    ga('send', 'pageview')
+</script>
+
+
+<script src="https://www.google-analytics.com/analytics.js" async defer></script>
+    </div>
+</div>
+</body>
+</html>
---
+++ wp-content/themes/gaeamagazine/parts/navbar.php
@@ -0,0 +1,28 @@
+<?php
+
+echo '<nav>
+
+    <ul>
+    <a href="index.php"><img id="title" src="img/logo.gaea.png" /></a>
+    <!--    <h1 id="title"><a href="index.php">GAEA</a></h1>-->
+        
+        <li class="mainnav"><a href="about.php">About</a></li>
+        <li class="mainnav"><a href="editions.php">Editions</a> </li>
+        <li class="mainnav"><a href="articles.php">Articles</a></li>
+        <li class="mainnav"><a href="contacts.php">Contacts</li></a></li>
+        <li class="mainnav"><a href="shop.php">Shop</a></li>
+        <li class="dropdown mainnav">Social
+            <div class="dropdown-content">
+                <ul>
+                    <li>Facebook</li>
+                    <li>Linked in</li>
+                    <li>Vimeo</li>
+                </ul>
+            </div>
+        </li>
+        <div id="language"><a>PT</a>/<a>ENG</a></div>
+    </ul>
+
+</nav>';
+
+?>
