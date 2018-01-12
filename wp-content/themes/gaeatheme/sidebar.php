92df538d593aad415eec7e593ee256a40048d18a
Ivo Amaro <ivo.amaro.97.p@gmail.com>
2018-01-12 00:53:52 +0000

    minor changes

--- wp-content/themes/gaeatheme/sidebar.php
+++ wp-content/themes/gaeatheme/sidebar.php
@@ -5,8 +5,6 @@
     <!--    <h1 id="title"><a href="index.php">GAEA</a></h1>-->
 
         <?php wp_list_pages( '&title_li=&exclude=2' ); ?>
-        <li class="page_item social"><a><i class="fa fa-facebook-square fa-2x" aria-hidden="true"></i></a></li>
-        <li class="page_item social"><a> <i class="fa fa-instagram fa-2x" aria-hidden="true"></i></a></li>
         <div id="language"><a>PT</a>/<a>ENG</a></div>
     </ul>
 
