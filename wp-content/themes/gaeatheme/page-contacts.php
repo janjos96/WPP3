c0ab0ac692db0216c019bebdd751db778828399c
Ivo Amaro <ivo.amaro.97.p@gmail.com>
2018-01-15 00:46:25 +0000

    asd

--- wp-content/themes/gaeatheme/page-contacts.php
+++ wp-content/themes/gaeatheme/page-contacts.php
@@ -36,7 +36,34 @@ get_header(); ?>
             endwhile; // End of the loop.
             ?>
 
+            <div id="address">
+                <p>Fell free to contact us anytime.</p>
+
+                <br>
+                <i class="fa fa-map-marker" aria-hidden="true"></i>
+                <p> Street Indústria do Papel, Nº 104 <br>
+                    3305-096 - Coimbra</p><br>
+                <i class="fa fa-phone" aria-hidden="true"></i>
+                <p> 21 239 329 1323</p>
+
+
+
+            </div>
+            <div class="emailform">
+                <i class="fa fa-envelope-o" aria-hidden="true"></i>
+                <form action="envia.php" method="POST">
+                    <input type="text" name="name" placeholder="Name" required><br>
+                    <input type="text" name="email" placeholder="Email" required><br>
+                    <input id="messageBox" type="text" name="message" placeholder="Message" required><br>
+                    <input class="inputButton" type="submit" value="Send">
+                    <input class="inputButton" type="reset" value="Reset">
+                </form>
+            </div>
+
         </main><!-- #main -->
+
+
+
     </div><!-- #primary -->
 
 
